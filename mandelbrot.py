fcf310d81b196c1e6ebc620f55b2e97731423fe0
Danny Lade <danny.lade@iteratec.com>
2023-12-31 15:11:46 +0100

    Forget about finding cycles but break early on it.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,37 +1,37 @@
 import matplotlib.pyplot as plt
+from math import sqrt
 
 # Erstellen eines Gitters von c-Werten
-epsilon = 1e-1
-iterations = 10
+iterations = 1000
 width, height = 800, 800
-xmin, xmax, ymin, ymax = -2, 2, -2, 2
-
-scale_factor_x = ((xmax - xmin) / width)
-scale_factor_y = ((ymax - ymin) / height)
+xmin, xmax, ymin, ymax = -2.0, 1.0, -sqrt(2.0), sqrt(2.0)
 
 converged = [[0 for _ in range(width)] for _ in range(height)]
 for y in range(height):
-    scale_y = ymin + y * scale_factor_y
+    scale_y = ymin + y * (ymax - ymin) / height
     for x in range(width):
-        scale_x = xmin + x * scale_factor_x
+        scale_x = xmin + x * (xmax - xmin) / width
 
         c = scale_x + scale_y * 1j
-        z = c
-        for i in range(1, iterations):
+        z, sum_z = c, 0j
+        for i in range(iterations):
             z = z**2 + c
-            if abs(z) > 2:
+
+            abs_z = abs(z)
+            if abs_z >= 2:
+                converged[y][x] = 50 if abs_z > 50 else abs_z
                 break
 
-            if abs(z - c) < epsilon:
-                converged[y][x] = i
-                break
+            sum_z += z
+            if i >= 10:
+                mean_z = abs(sum_z / (i + 1))
+                if mean_z < 1.0:
+                    converged[y][x] = 0
+                    break
 
-#            converged[y][x] = i
-
-print(0, max(max(converged)))
 
 # Visualisierung
-plt.imshow(converged, extent=(xmin, xmax, ymin, ymax))
+plt.imshow(converged, extent=(xmin, xmax, ymin, ymax), cmap='grey')
 plt.xlabel('Realteil')
 plt.ylabel('Imaginärteil')
 plt.title('Mandelbrot-Set')
