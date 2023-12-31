59678bdeb4d6c9f6c917beeb40dea227807825e2
Danny Lade <danny.lade@iteratec.com>
2023-12-31 14:52:31 +0100

    Trying to show cycles is buggy, but looks quite funny.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,32 +1,37 @@
 import matplotlib.pyplot as plt
 
 # Erstellen eines Gitters von c-Werten
+epsilon = 1e-1
+iterations = 10
 width, height = 800, 800
 xmin, xmax, ymin, ymax = -2, 2, -2, 2
 
+scale_factor_x = ((xmax - xmin) / width)
+scale_factor_y = ((ymax - ymin) / height)
 
-def scale_x(x):
-    return xmin + ((xmax - xmin) * x / width)
-
-
-def scale_y(y):
-    return ymin + ((ymax - ymin) * y / width)
-
-
-iterations = 10
-divergiert = [[False for _ in range(width)] for _ in range(height)]
+converged = [[0 for _ in range(width)] for _ in range(height)]
 for y in range(height):
+    scale_y = ymin + y * scale_factor_y
     for x in range(width):
-        c = scale_x(x) + scale_y(y) * 1j
+        scale_x = xmin + x * scale_factor_x
+
+        c = scale_x + scale_y * 1j
         z = c
-        for i in range(iterations):
+        for i in range(1, iterations):
             z = z**2 + c
             if abs(z) > 2:
-                divergiert[y][x] = True
                 break
 
+            if abs(z - c) < epsilon:
+                converged[y][x] = i
+                break
+
+#            converged[y][x] = i
+
+print(0, max(max(converged)))
+
 # Visualisierung
-plt.imshow(divergiert, extent=(xmin, xmax, ymin, ymax))
+plt.imshow(converged, extent=(xmin, xmax, ymin, ymax))
 plt.xlabel('Realteil')
 plt.ylabel('Imaginärteil')
 plt.title('Mandelbrot-Set')
